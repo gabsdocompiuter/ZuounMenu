7fc01515812e53433979f614a163ef9472fb3f2b
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-14 22:05:27 -0300

    Ajustava versão no NetFramework e criado aquivo Util

---
+++ ZuounSystem/ZuounSystem/Util.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZuounSystem
+{
+    public static class Util
+    {
+        #region Validação de Campos
+
+        public static bool CampoPreenchido(this TextBox obj, string campo)
+        {
+            //Se o campo está preenchido, retorna verdadeiro
+            if (!string.IsNullOrEmpty(obj.Text)) return true;
+
+            MessageBox.Show($"Campo '{campo}' não preenchido!");
+            obj.Focus();
+            return false;
+        }
+
+        #endregion
+    }
+}
