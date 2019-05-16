d923265078c9d4bca22d65cfda4c93de7a8a0e8a
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-16 18:48:44 -0300

    Adiciona resize ao CadastroPadrao

--- ZuounSystem/ZuounSystem/Cadastros/FrmCadastroPadrao.cs
+++ ZuounSystem/ZuounSystem/Cadastros/FrmCadastroPadrao.cs
@@ -38,6 +38,33 @@ namespace ZuounSystem.Cadastros
 
         #endregion
 
+        #region Definições para o resize
+
+        private const int cGrip = 16;
+        private const int cCaption = 32;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x84)
+            {
+                Point pos = new Point(m.LParam.ToInt32());
+                pos = this.PointToClient(pos);
+                if (pos.Y < cCaption)
+                {
+                    m.Result = (IntPtr)2;
+                    return;
+                }
+                if (pos.X >= this.ClientSize.Width - cGrip && pos.Y >= this.ClientSize.Height - cGrip)
+                {
+                    m.Result = (IntPtr)17;
+                    return;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        #endregion
+
         public FrmCadastroPadrao()
         {
             InitializeComponent();
