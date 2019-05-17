9df5a369496eb727c737b61998fc701b933c02b1
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-16 23:17:48 -0300

    Cria classe para buscar opções do menu

---
+++ ZuounSystem/Database/Menu/MenuPrincipal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Menu
+{
+    public class MenuPrincipal
+    {
+        private readonly MySQL my;
+
+        public MenuPrincipal()
+        {
+            my = new MySQL();
+        }
+
+        public ArrayList GetOpcoes()
+        {
+            ArrayList array = new ArrayList();
+
+            string sql = "SELECT * FROM menu";
+
+            my.ExecuteReader(sql);
+
+            if (my.HasRows())
+            {
+                while (my.ReadNextRecord())
+                {
+                    MenuPrincipalDTO dto = new MenuPrincipalDTO
+                    {
+                        Descricao = my.GetString("descricao"),
+                        Opcao = my.GetString("opcao")
+                    };
+                    array.Add(dto);
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ ZuounSystem/Database/Menu/MenuPrincipalDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Menu
+{
+    public class MenuPrincipalDTO
+    {
+        public string Descricao { get; set; }
+        public string Opcao { get; set; }
+    }
+}
